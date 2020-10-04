a1420487fb13bf23dbaa3f62a6c6c71ddd5a69a4
KoniSavara <72244241+KoniSavara@users.noreply.github.com>
2020-10-04 22:05:54 +0300

    Валюты

---
+++ ConsoleApplication1228.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int x;
+	cout << "1.Доллар \n2.Гривна \n3.Евро \n4.Гроши" << endl;
+	cin >> x;
+	if (x > 4) {
+		cout << "Введите значение от 1 до 4" << endl;
+		cin >> x;
+	}
+	switch (x) {
+	case 1:
+		cout << "Доллар - почти по 100, вы нормальные вообще?" << endl;
+		break;
+	case 2:
+		cout << "Гривна - Украина це Европа!" << endl;
+		break;
+	case 3:
+		cout << "Евро - Коллективная Валюта Европейских стран, как бы кайф ежжи." << endl;
+		break;
+	case 4:
+		cout << "ГРОШИ - национальная еврейская валюта, жиды лютуют))" << endl;
+		break;
+	}
+
+system("pause");
+return(0);
+}
