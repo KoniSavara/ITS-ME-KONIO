134f640e937d1aac8108b768acfb9e901f7ed1de
KoniSavara <72244241+KoniSavara@users.noreply.github.com>
2020-10-04 20:40:35 +0300

    Ну вот я сделяль
    ))

---
+++ Вампиры и Дураки.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    int a;
+    int b;
+    cout << "Write 2 Numbers: " << endl;
+    cin >> a;
+    cin >> b;
+    cout << "Summa Chisel: " << a + b << endl;
+
+    system("pause");
+    return 0;
+}
+
